77f669f576d7df3562491ca71785616a9a020787
Shashanka-rao <raobnshashank567@gmail.com>
2023-06-04 00:22:09 +0530

    Home page updated

--- MiniProjectMAD1/app/src/main/java/com/example/miniprojectmad1/Home.java
+++ MiniProjectMAD1/app/src/main/java/com/example/miniprojectmad1/Home.java
@@ -3,12 +3,60 @@ package com.example.miniprojectmad1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
+    ImageView dashBoardBtn,contactUsBtn,carImage;
+    TextView bookServiceBtn,serviceHistoryBtn,serviceCostBtn,trackServiceBtn;
+    public int flag=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        bookServiceBtn = findViewById(R.id.bookServiceBtn);
+        serviceCostBtn = findViewById(R.id.serviceCostBtn);
+        serviceHistoryBtn = findViewById(R.id.serviceHistoryBtn);
+        trackServiceBtn = findViewById(R.id.trackServiceBtn);
+
+        dashBoardBtn = findViewById(R.id.dashBoardBtn);
+        contactUsBtn = findViewById(R.id.contactUsBtn);
+        carImage = findViewById(R.id.carImage);
+
+        //default condition
+        bookServiceBtn.setVisibility(View.INVISIBLE);
+        serviceCostBtn.setVisibility(View.INVISIBLE);
+        serviceHistoryBtn.setVisibility(View.INVISIBLE);
+        trackServiceBtn.setVisibility(View.INVISIBLE);
+
+
+            dashBoardBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(flag==0) {
+                        carImage.setVisibility(View.INVISIBLE);
+                        bookServiceBtn.setVisibility(View.VISIBLE);
+                        serviceCostBtn.setVisibility(View.VISIBLE);
+                        serviceHistoryBtn.setVisibility(View.VISIBLE);
+                        trackServiceBtn.setVisibility(View.VISIBLE);
+                        flag = 1;
+                    }
+                    else if(flag==1)
+                    {
+                        bookServiceBtn.setVisibility(View.INVISIBLE);
+                        serviceCostBtn.setVisibility(View.INVISIBLE);
+                        serviceHistoryBtn.setVisibility(View.INVISIBLE);
+                        trackServiceBtn.setVisibility(View.INVISIBLE);
+                        carImage.setVisibility(View.VISIBLE);
+                        flag = 0;
+                    }
+                }
+            });
+
+
+
     }
 }
